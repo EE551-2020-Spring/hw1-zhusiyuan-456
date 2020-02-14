d26e3c36a37d85dfdcdee7599ba51404a4ebf979
zhusiyuan-456 <55150278+zhusiyuan-456@users.noreply.github.com>
2020-02-13 22:54:45 -0500

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,26 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> def numbers_and_strings():
+	x="EE551"
+	y="Stevens"
+	z=5*y
+	length=len(z)
+	m=y+" is good"
+	n=m.replace("good","awesome")
+	return x,y,z,length,m,n
+
+>>> 
+>>> def list():
+	n="Stevens is awesome"
+	p=n.split(", ",17)
+	r=p[3:-1]
+	first_row=[1,4,5]
+	second_row=[6,10,11]
+	third_row=[12,17,38]
+	c=('first_row[2]','second_row[2]','third_row[2]')
+	(x,y,z)=c
+	for elm in first_row
+	
+SyntaxError: invalid syntax
+>>> 
