d76d8f053572ec9d0e1a648addf2bd893c376b96
zhusiyuan-456 <55150278+zhusiyuan-456@users.noreply.github.com>
2020-02-25 09:29:11 -0500

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,5 +1,4 @@
-Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> import math
 >>> def numbers_and_strings():
 	x="EE551"
@@ -10,28 +9,19 @@ Type "help", "copyright", "credits" or "license()" for more information.
 	n=m.replace("good","awesome")
 	return x,y,z,length,m,n
 
-
->>> def list():
-	n="Stevens is awesome"
-	p=n.split(", ",17)
-	r=p[3:-1]
-	first_row=[1,4,5]
-	second_row=[6,10,11]
-	third_row=[12,17,38]
-	c=('first_row[2]','second_row[2]','third_row[2]')
-	(x,y,z)=c
-	s = ("stevens") 
-l1 = [] 
-l2 = []
-for c in s:  
-    l1.append(c) 
-    l2.append(ord(c))
-
-	return p,r,c,o
+def lists():
+    n = "Stevens is awesome"
+    p= n.split(' ')
+    r=p[2][1:]
+    A=[[1,4,5],[6,10,11],[12,17,38]]
+    c =[row[2] for row in A]
+    d=[A[i][2-i] for i in [0,1,2]if(A[i][2-i]%2==0)]
+    o=[[i,ord(i)] for i in['S','t','e','v','e','n','s']]
+    return p, r, c, d, o
 
  def dictionaries():
  a={"fruit":"apple","quantity":4,"color":"green"}
-f=a["fruit"]
-p={"first_name":"Grace",'last_name':'Hopper','jobs':['scientist','engineer'],'age':85}
-p['jobs'].append("programmer")
-return a,f,p
+ f=a["fruit"]
+ p={"first_name":"Grace",'last_name':'Hopper','jobs':['scientist','engineer'],'age':85}
+ p['jobs'].append("programmer")
+ return a,f,p
